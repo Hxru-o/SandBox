a739672c7f0db2a51199195382828241d2b99dc4
Hxru-o <102019445+Hxru-o@users.noreply.github.com>
2022-05-19 20:57:42 +0900

    add Craft.cs

--- Script/UI/Craft.cs
+++ Script/UI/Craft.cs
@@ -15,17 +15,30 @@ public class Craft : MonoBehaviour
     private bool isActivated = false;
     private bool isPreviewActivated = false;
 
+    //GameObject
     [SerializeField]
     private GameObject go_BaseUI; // BaseUI
-    [SerializeField]
-    private CraftBox[] craft_Box;
     private GameObject go_Preview; //prefab preview
     [SerializeField]
+    private GameObject go_prefab; // original prefab
+    
+    [SerializeField]
+    private CraftBox[] craft_Box;
+    
+    [SerializeField]
     private Transform tf_Player;
+
+    // raycast
+    private RaycastHit hitinfo;
+    [SerializeField]
+    private LayerMask layerMask;
+    [SerializeField]
+    private float range;
 
     public void SlotClick(int _slotNumber)
     {
       go_Preview = Instantiate(craft_Box[_slotNumber].go_PreviewPrefab, tf_Player.position + tf_Player.forward, Quaternion.identity);
+      go_prefab = craft_Box[_slotNumber].go_Prefab;
 
       isPreviewActivated = true;
       go_BaseUI.SetActive(false);
@@ -33,16 +46,52 @@ public class Craft : MonoBehaviour
     
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Tab))
+        if(Input.GetKeyDown(KeyCode.Tab) && !isPreviewActivated)
         {
             Window();
         }
 
+        if(isPreviewActivated)
+         {
+           PreviewPositionUpdate();
+         }  
+        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Build();
+        }
+     
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             Cancel();
         }
     }
+
+    private void Build()
+    {
+        if(isPreviewActivated)
+        {
+            Instantiate(go_prefab, hitinfo.point, Quaternion.identity);
+            Destroy(go_Preview);
+            isActivated = false;
+            isPreviewActivated = false;
+            go_Preview = null;
+            go_prefab = null;
+        }
+    }
+
+    private void PreviewPositionUpdate()
+    {
+      if(Physics.Raycast(tf_Player.position, tf_Player.forward, out hitinfo, range, layerMask))
+      {
+          if(hitinfo.transform != null)
+          {
+              Vector3 _location = hitinfo.point;
+              go_Preview.transform.position = _location;
+          }
+      }
+    }
+
 
     private void Cancel()
     {
@@ -52,6 +101,7 @@ public class Craft : MonoBehaviour
        isActivated = false;
        isPreviewActivated = false;
        go_Preview = null;
+       go_prefab = null;
 
        go_BaseUI.SetActive(false);
     }
