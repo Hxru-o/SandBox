395a432d3e52b37603c3d7e459dd0f761381a853
Hxru-o <102019445+Hxru-o@users.noreply.github.com>
2022-05-25 18:24:34 +0900

    Add LobbyManger - Photon

---
+++ Script/Network/LobbyManager.cs
@@ -0,0 +1,18 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
